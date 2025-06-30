a6a9cd08435dbc134c38a77291abf65f3c3e21c4
Luis Felipe <lfchiqueto@gmail.com>
2025-06-30 19:20:15 +0000

    feat: add userRegisterDTO

---
+++ backend/src/main/java/com/organizaAi/OrganizaAi/dto/UserRegisterDTO.java
@@ -0,0 +1,50 @@
+package com.organizaAi.OrganizaAi.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.CPF;
+import jakarta.validation.constraints.Pattern;
+
+
+public record LoginDTO (
+    @NotNull
+    @NotEmpty
+    String name,
+    @NotNull    
+    @NotEmpty
+    @Email
+    String email,
+    @NotNull
+    @NotEmpty
+    String password,
+    @NotNull
+    @NotEmpty
+    //@CPF
+    String cpf,
+    @NotNull
+    @NotEmpty
+    String rg,
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "Phone must be in the format (XX) XXXXX-XXXX or (XX) XXXX-XXXX")
+    String phone,
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "\\d{5}-\\d{3}|\\d{8}", message = "CEP must be in the format XXXXX-XXX or XXXXXX")
+    String cep,
+    @NotNull
+    @NotEmpty
+    String city,
+    @NotNull
+    @NotEmpty
+    String state,
+    @NotNull
+    String birthDate
+
+    
+    
+    ) {
+    
+}
